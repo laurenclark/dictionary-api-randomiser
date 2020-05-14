0de24559c3d30db611ed8c0344529e601baca29b
Lauren <lauren.c4231@gmail.com>
2020-05-14 16:36:18 +0100

    Scaffold out Queries

--- src/views/Queries.vue
+++ src/views/Queries.vue
@@ -1,10 +1,38 @@
 <template>
-    <div></div>
+    <div class="word-display">
+        <h1>Word Randomiser</h1>
+        <v-button>Randomise!</v-button>
+        <section>
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+            <word-span :text="'Word'" />
+        </section>
+    </div>
 </template>
 
 <script>
-export default {};
+import Button from '@/components/Button.vue';
+import WordSpan from '@/components/WordSpan.vue';
+export default {
+    name: 'Queries',
+    components: {
+        'v-button': Button,
+        'word-span': WordSpan
+    }
+};
 </script>
 
-<style scoped>
+
+<style scoped lang="scss">
+.word-display {
+    max-width: 1200px;
+    margin: 0 auto;
+}
 </style>
