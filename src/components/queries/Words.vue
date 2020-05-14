9e8f63a9df7da0338f538140be272c8d7f08a8a3
Lauren <lauren.c4231@gmail.com>
2020-05-14 22:19:10 +0100

    Fetch random words on mount

--- src/components/queries/Words.vue
+++ src/components/queries/Words.vue
@@ -1,6 +1,16 @@
 <template>
     <section class="word-display">
-        <word-span :text="'Word'" />
+        <div v-if="words">
+            <span v-for="(word, index) in words" :key="index">
+                <word-span :text="word" />
+            </span>
+        </div>
+        <span v-else>
+            <p>
+                Click the
+                <b>Randomise</b> button to start!
+            </p>
+        </span>
     </section>
 </template>
 
@@ -11,11 +21,32 @@ export default {
     components: {
         'word-span': WordSpan
     },
-    props: {
-        class: String
+    data() {
+        return {
+            words: ''
+        };
+    },
+    mounted() {
+        this.getRandomWords(10);
+    },
+    methods: {
+        async getRandomWords(wordAmount = 10) {
+            try {
+                const url = `https://random-word-api.herokuapp.com/word?number=${wordAmount}`;
+                const res = await fetch(url);
+                const data = await res.json();
+                this.words = data;
+            } catch (error) {
+                console.error('getRandomWords has failed!');
+                console.error(error.response.status);
+            }
+        }
     }
 };
 </script>
 
 <style scoped>
+.word-display span {
+    font-size: 0.8rem;
+}
 </style>
