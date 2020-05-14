4a8a60359b912c1e7d33785d1296c406eea42f11
Lauren <lauren.c4231@gmail.com>
2020-05-14 16:36:40 +0100

    Add WordSpan component

---
+++ src/components/WordSpan.vue
@@ -0,0 +1,16 @@
+<template>
+    <span class="badge badge-large">{{text}}</span>
+</template>
+
+<script>
+export default {
+    name: 'WordSpan',
+    props: {
+        text: { type: String },
+        id: { type: Number }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
