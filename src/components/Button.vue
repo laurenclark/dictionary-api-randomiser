ca3a6fd59b282a506ffb76078d99e6898da9794c
Lauren <lauren.c4231@gmail.com>
2020-05-14 16:36:33 +0100

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="button button--large">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button'
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
