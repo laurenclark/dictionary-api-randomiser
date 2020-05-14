ce7cea77249e353bf89fdc954d6ae1900bcb3f76
Lauren <lauren.c4231@gmail.com>
2020-05-14 11:59:15 +0100

    🎉Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <WordDisplay />
+    </div>
+</template>
+
+<script>
+import WordDisplay from '@/components/WordDisplay.vue';
+
+export default {
+    name: 'WordDisplay',
+    components: {
+        WordDisplay
+    }
+};
+</script>
