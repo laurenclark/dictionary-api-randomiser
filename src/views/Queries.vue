a1c4a2d7bcf47cfc721cedc98b5f409ff3affe2a
Lauren <lauren.c4231@gmail.com>
2020-05-15 01:31:53 +0100

    Move Results to be a child of Words

--- src/views/Queries.vue
+++ src/views/Queries.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="queries">
         <word-display />
-        <results />
     </div>
 </template>
 
 <script>
 import Words from '@/components/queries/Words.vue';
-import Results from '@/components/queries/Results.vue';
+
 export default {
     name: 'Queries',
     components: {
-        'word-display': Words,
-        results: Results
+        'word-display': Words
     }
 };
 </script>
