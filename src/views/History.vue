3e8a53478923ba37b265f7a1527d9315153f3f4c
Lauren <lauren.c4231@gmail.com>
2020-05-15 14:55:24 +0100

    Add TODOS

--- src/views/History.vue
+++ src/views/History.vue
@@ -3,6 +3,13 @@
 </template>
 
 <script>
+// TODO:
+// Store words clicked on and searched for next to the date/time
+// Show how many times a word was searched for next to the word
+// Show how many definitons it returned if any, if not show 0 or a "no results"
+// Add this to the Store so that Statistics can access the data
+// Persist this data with localStorage or GraphQL
+
 export default {
     name: 'History'
 };
