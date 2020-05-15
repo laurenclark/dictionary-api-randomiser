b107fca1f7307834f529ba4b964a8f990e346ed7
Lauren <lauren.c4231@gmail.com>
2020-05-15 14:50:38 +0100

    Add TODOS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,13 @@
     <div>{{ $route.name }}</div>
 </template>
 
+
 <script>
+//TODO:
+// Use vue-chart to render out a pie chart and bar graph
+// Clicking on the pie chart sections for each {type} should show {type.length} from
+// history. User can see amount of nouns, verbs etc.
+
 export default {
     name: 'Stats'
 };
